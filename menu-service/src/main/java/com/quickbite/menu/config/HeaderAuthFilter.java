9b95e03d549f5ed4f4ff3e6c7276562948fe9943
Nikhil Chahar <nikhilchahar592@gmail.com>
2026-05-13 16:44:07 +0530

    feat: implemented menu-service microservice

---
+++ menu-service/src/main/java/com/quickbite/menu/config/HeaderAuthFilter.java
@@ -0,0 +1,43 @@
+package com.quickbite.menu.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+// This filter reads the X-User-Role header that Gateway added
+@Component
+public class HeaderAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String role = request.getHeader("X-User-Role");
+        String userId = request.getHeader("X-User-Id");
+
+        if (role != null && userId != null) {
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(
+                            userId,
+                            null,      // credentials (not needed)
+                            List.of(new SimpleGrantedAuthority("ROLE_" + role))
+                    );
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
